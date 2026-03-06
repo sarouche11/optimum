ff84e5e0fd82b60e21d071db806d6472f33e66f6
sarah <sarouc11@gmail.com>
2026-03-06 12:18:18 +0100

    update

--- main/urls.py
+++ main/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('panelOptimumDZJRJIZJIEJRIDHFRG/', admin.site.urls),
     path("", include("authentification.urls")),
     path('root/', include("root.urls")),
     path('captcha/', include('captcha.urls')),
